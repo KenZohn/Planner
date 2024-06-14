c8ec981dbcd2ba6215b721b755c3a3e78030f081
Johnny <125989975+KenZohn@users.noreply.github.com>
2024-06-13 22:34:45 -0300

    Add files via upload
    Banco de Dados atualizado

--- planner.sql
+++ planner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/06/2024 às 16:14
+-- Tempo de geração: 14/06/2024 às 03:10
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -99,17 +99,23 @@ CREATE TABLE `gastos` (
 --
 
 INSERT INTO `gastos` (`cod_gastos`, `cod_usuario`, `tipo_gastos`, `ano`, `mes`, `linha_item`, `descricao`, `valor`) VALUES
-(1, 6, 'fixo', 0, 6, 0, 'Aluguel', 668.8),
+(1, 6, 'fixo', 0, 6, 0, 'Aluguel', 670),
 (2, 6, 'fixo', 0, 6, 1, 'Internet', 94.9),
 (3, 6, 'fixo', 0, 6, 2, 'Celular', 33),
 (4, 6, 'fixo', 0, 6, 3, 'Água', 38.8),
 (5, 6, 'fixo', 0, 6, 4, 'Luz', 45),
-(6, 6, 'mercado', 0, 6, 0, 'Máximo', 45.8),
+(6, 6, 'mercado', 0, 6, 0, 'Alimentos', 45.8),
 (7, 6, 'mercado', 0, 6, 1, 'Whey Protein', 75),
 (8, 6, 'fixo', 0, 5, 0, 'Aluguel', 670),
 (9, 6, 'fixo', 0, 5, 1, 'Água', 38),
 (10, 6, 'fixo', 0, 5, 2, 'Luz', 45),
-(11, 6, 'lazer', 0, 6, 0, 'Cinema', 40);
+(11, 6, 'lazer', 0, 6, 0, 'Cinema', 40),
+(12, 6, 'fixo', 0, 1, 0, 'Aluguel', 670),
+(13, 6, 'fixo', 0, 2, 0, 'Aluguel', 670),
+(14, 6, 'fixo', 0, 1, 1, 'Internet', 94.9),
+(15, 6, 'fixo', 0, 1, 2, 'Celular', 33),
+(16, 6, 'fixo', 0, 1, 3, 'Água', 38),
+(17, 6, 'fixo', 0, 1, 4, 'Luz', 45);
 
 -- --------------------------------------------------------
 
@@ -132,7 +138,7 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`cod_usuario`, `nome`, `email`, `login`, `senha`) VALUES
 (0, 'Aladim', 'lampadamagica123@email.com', 'admin', '123456'),
 (2, 'Thais', 'thais.ferreira@email.com', 'thaisf', '354679'),
-(6, 'Johnny', 'johnny.bravo@email.com', 'johnny', '1');
+(6, 'Johnny Mukai', 'johnny.bravo@email.com', 'johnny', '1');
 
 --
 -- Índices para tabelas despejadas
@@ -184,7 +190,7 @@ ALTER TABLE `calendario`
 -- AUTO_INCREMENT de tabela `gastos`
 --
 ALTER TABLE `gastos`
-  MODIFY `cod_gastos` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `cod_gastos` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
