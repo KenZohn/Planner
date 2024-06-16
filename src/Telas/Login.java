d957313c0caa86ee140d9649e2465da1ac5a6487
thais <thais.ferreira1795@gmail.com>
2024-06-16 14:43:17 -0300

    criar uma nova tela

--- src/Telas/Login.java
+++ src/Telas/Login.java
@@ -17,6 +17,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         cadastrar = new Cadastrar();
+        redefinir = new Redefinir();
 
         setLocationRelativeTo(null);
     }
